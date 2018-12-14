8c776bcf90966dee4d49254f76f0334375b36c53
MuriloCunha <mcscunha@yahoo.com.br>
2018-12-14 20:27:35 -0200

    Disciplinas e Disponibilidades

---
+++ Grade GMPLv1.1/DDL/cursos.sql
@@ -0,0 +1,2 @@
+insert into grade_horario_cursos (nome) values ('ADS');
+insert into grade_horario_cursos (nome) values ('GNI');
---
+++ Grade GMPLv1.1/DDL/periodo.sql
@@ -0,0 +1,3 @@
+insert into grade_horario_periodos (periodo) values ('Manha');
+insert into grade_horario_periodos (periodo) values ('Tarde');
+insert into grade_horario_periodos (periodo) values ('Noite');
